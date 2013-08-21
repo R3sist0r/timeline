6b8293ac81562b3e392e48eeb90a676655e785ec
Reuben Posthuma <reuben.posthuma@gmail.com>
2013-08-21 22:45:57 +1200

    Got the system taking JSON from the db and making a timeline :)

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,12 +2,19 @@ class EventsController < ApplicationController
 	def index
 
 		@json = {:timeline => { :date=>[], :era=>[] } }
-		events = Event.all()
+		@json[:timeline][:headline] = "The Main Timeline Headline Goes here"
+		@json[:timeline][:type] = "default"
+		@json[:timeline][:text] = "<p>Intro body text goes here, some HTML is ok</p>"
+		@json[:timeline][:asset] = {}
+		@json[:timeline][:asset][:media] = "http://yourdomain_or_socialmedialink_goes_here.jpg"
+		@json[:timeline][:asset][:credit] = "Credit"
+		@json[:timeline][:asset][:caption] = "caption"
 
+		events = Event.all()
 		events.each do |date|
 			@date = {}
-			@date[:startDate] = date.startDate
-			@date[:endDate] = date.endDate
+			@date[:startDate] = date.startDate.to_s
+			@date[:endDate] = date.endDate.to_s
 			@date[:headline] = date.headline
 			@date[:text] = date.text
 			@date[:tag] = date.tag
@@ -19,7 +26,7 @@ class EventsController < ApplicationController
 			@date[:asset][:caption] = date.caption
 			@json[:timeline][:date].push(@date)
 		end
-		@json =@json.to_json
+		@json =@json.to_json.html_safe
 
 
 	end
